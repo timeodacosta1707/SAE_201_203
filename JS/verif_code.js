603206c3edd6f30220691bb2d2abb5cc4ad8c996
timeodacosta1707 <timeo.da-costapedu.univ-eiffel.fr>
2025-04-13 16:58:05 +0200

    Fichiers pour le mot de passe oublié

---
+++ JS/verif_code.js
@@ -0,0 +1,28 @@
+const inputs = document.querySelectorAll('.case');
+const boutonEnvoyer = document.getElementById("bouton-envoyer");
+
+inputs[0].focus();
+
+function verifierChamps() {
+    const tousRemplis = Array.from(inputs).every(input => input.value.length === 1);
+    boutonEnvoyer.disabled = !tousRemplis;
+}
+
+verifierChamps();
+
+inputs.forEach((input, index) => {
+    input.addEventListener('input', function (e) {
+        const value = e.target.value;
+
+        if (value && index < inputs.length - 1) {
+            inputs[index + 1].focus();
+        }
+        verifierChamps();
+    });
+
+    input.addEventListener('keydown', function (e) {
+        if (e.key === 'Backspace' && !input.value && index > 0) {
+            inputs[index - 1].focus();
+        }
+    });
+}); 
